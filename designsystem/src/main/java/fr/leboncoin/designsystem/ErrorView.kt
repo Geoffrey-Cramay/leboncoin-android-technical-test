0d1b9e89bca3786a78e9ba1a3d9b4284265d7907
Geoffrey-Cramay <geoffrey.cramay@gmail.com>
2026-08-05 20:08:16 +0200

    feat: add Loader and ErrorView composables to :designsystem

---
+++ designsystem/src/main/java/fr/leboncoin/designsystem/ErrorView.kt
@@ -0,0 +1,22 @@
+package fr.leboncoin.designsystem
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.adevinta.spark.SparkTheme
+
+@Composable
+fun ErrorView(message: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = message,
+            style = SparkTheme.typography.body1,
+        )
+    }
+}
---
+++ designsystem/src/main/java/fr/leboncoin/designsystem/Loader.kt
@@ -0,0 +1,18 @@
+package fr.leboncoin.designsystem
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.adevinta.spark.components.progress.Spinner
+
+@Composable
+fun Loader(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Spinner()
+    }
+}
